a7e7e967161fb38b560f1b03500bb6fb0333650c
Devin Udy <devin.udy@gmail.com>
2019-09-04 14:20:48 -0600

    Switched images based upon random number

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 function newPhoto() {
-    document.getElementsByClassName("photo")[0].src = "photo2.jpg"
+    document.getElementsByClassName("photo")[0].src = "" + photoGenerator() + ".jpg"
 }
 
 // function newPhoto() {
 //     document.getElementsByClassName("photo").src = "photo2.jpg"
 // }
+
+// Create an array with just the name of the photos file
+var photoArray = ["photo", "photo2"];
+// Then create random number generator to randomly pick photo
+function photoGenerator() {
+    photoArray;
+    var randomPhoto = Math.floor(Math.random() * photoArray.length)
+    return photoArray[randomPhoto];
+}
+// Adjust newPhoto() accordingly
